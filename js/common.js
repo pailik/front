bf38d2ad9ed1bc615bcdb5442c0f59e541b3c649
iMega <info@imega.ru>
2016-08-21 22:11:14 +0300

    add foundation

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-var kubikApp = angular.module('kubikApp', ['ngRoute'], function ($interpolateProvider) {
+var kubikApp = angular.module('kubikApp', ['ngRoute', 'ui.router'], function ($interpolateProvider) {
     $interpolateProvider.startSymbol('{|').endSymbol('|}');
 });
 
@@ -6,12 +6,38 @@ kubikApp.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 });
 
+kubikApp.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('promo', {
+
+            url: "/",
+            templateUrl: "view/index.html"
+        })
+        .state('task', {
+            url: '/task',
+            templateUrl: "view/task.html"
+        });
+        $urlRouterProvider.otherwise('/');
+    }
+]);
+
 kubikApp.controller('signupCtrl', ['$http', '$location', function ($http, $location) {
     this.activate = function () {
         if ($location.search().hasOwnProperty('code')) {
             this.code = true;
-            var token = $location.search()['code'];
-            $http.get('http://api.kubikvest.xyz/token/' + token);
+            var code = $location.search()['code'];
+            $http.get('http://api.kubikvest.xyz/auth/' + code);
+        }
+    }
+}]);
+
+kubikApp.controller('taskCtrl', ['$http', '$location', function ($http, $location) {
+    this.token = function () {
+        if ($location.search().hasOwnProperty('t')) {
+            var token = $location.search()['t'];
+            $http.get('http://api.kubikvest.xyz/token?t=' + token);
         }
     }
 }]);
